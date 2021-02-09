8737b8cb8074063e12e02e463c8ef03efc75b1a9
shinara03 <shinara03@gmail.com>
2021-02-08 18:02:58 -0800

    finish reducer

--- full_stack_project/frontend/fsp.jsx
+++ full_stack_project/frontend/fsp.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
   ReactDOM.render(<h1>WELCOME!</h1>, root);
   window.APIUtil = APIUtil;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 })
---
+++ full_stack_project/frontend/reducers/session_reducer.js
@@ -0,0 +1,19 @@
+import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from '../actions/session_actions'
+
+const _null = Object.freeze({
+  id: null
+})
+
+const sessionReducer = (state=_null, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER: 
+      return {id: action.currentUser.id};
+    case LOGOUT_CURRENT_USER:
+      return _null;
+    default:
+      return state;
+  }
+}
+
+export default sessionReducer;
