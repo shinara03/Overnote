9a316a9de0ba36b2a2c0a393ce895fae64d9d36d
shinara03 <shinara03@gmail.com>
2021-02-10 09:55:28 -0800

    start working on demo user

---
+++ frontend/components/demo/demo_container.js
@@ -0,0 +1,11 @@
+// import { connect } from 'react-redux';
+
+// import { login } from '../../actions/session_actions';
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     processForm: (user) => dispatch(login(user))
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SessionForm)
