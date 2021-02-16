49511df3a065c439b9eaaf5c61cce98bd82cfe6b
shinara03 <shinara03@gmail.com>
2021-02-16 11:04:01 -0800

    add seeds to notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,14 @@ user3 = User.create({email: 'helloWorld@gmail.com', password: "123456"})
 notebook1 = Notebook.create({notebook_name: 'first notebook', author_id: user1.id})
 notebook2 = Notebook.create({notebook_name: 'second notebook', author_id: user1.id})
 notebook3 = Notebook.create({notebook_name: 'third notebook', author_id: user1.id})
+
+#Notes 
+# Notes under notebook1 
+note1 = Note.create({title: 'note1', body: 'note 1 body', notebook_id: notebook1.id})
+note2 = Note.create({title: 'note2', body: 'note 2 body', notebook_id: notebook1.id})
+note3 = Note.create({title: 'note3', body: 'note 3 body', notebook_id: notebook1.id})
+
+# Notes under notebook2
+note4 = Note.create({title: 'Hello', body: 'note4 body', notebook_id: notebook2.id})
+note5 = Note.create({title: 'World', body: 'note5 body', notebook_id: notebook2.id})
+note6 = Note.create({title: 'Javascript', body: 'note 6 body', notebook_id: notebook2.id})
